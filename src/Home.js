9e5375c7d88711647713401f969c83344a15f9f2
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2022-07-06 19:47:40 -0400

    home page design and 3d model scale change

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,42 @@
 import React from "react";
 import "./Home.css";
+import Typewriter from "typewriter-effect";
+import ThreeRenders from "./ThreeRenders";
 export default function Home() {
   return (
     <div>
-      <h2>Hi,</h2>
-      <h2>I'm Arsalaan,</h2>
-      <h2>Web Developer</h2>
+      <ThreeRenders />
+      <div className="title-holder">
+        <p className="hi name">Hi, I'm </p>
+        {/* <p className="name"></p> */}
+        <TypeText />
+      </div>
     </div>
   );
 }
+
+const TypeText = () => {
+  return (
+    <Typewriter
+      options={{
+        loop: true,
+        wrapperClassName: "typewriter",
+        cursorClassName: "cursor",
+      }}
+      onInit={(typewriter) => {
+        typewriter
+          .typeString("Web Developer.")
+          .pauseFor(4000)
+          .deleteAll()
+          .typeString("Student.")
+          .pauseFor(4000)
+          .deleteAll()
+          .start();
+      }}
+    />
+  );
+};
+
+const onNode = () => {
+  console.log("hello");
+};
--- src/ThreeRenders.js
+++ src/ThreeRenders.js
@@ -1,16 +1,36 @@
-import { React, useRef } from "react";
+import { React, useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-
+import "./Three.css";
 export default function ThreeRenders() {
   function Box() {
     const myMesh = useRef();
+    const [shrinkObject, setShrinkObject] = useState(false);
+    const [growObject, setGrowObject] = useState(false);
 
     useFrame(({ clock }) => {
-      myMesh.current.rotation.y = clock.getElapsedTime();
+      myMesh.current.rotation.y = -clock.getElapsedTime();
+      if (shrinkObject) {
+        myMesh.current.scale.multiplyScalar(0.8);
+        if (myMesh.current.scale.x <= 0.01) {
+          setShrinkObject(false);
+          setGrowObject(true);
+        }
+      }
+      if (growObject) {
+        myMesh.current.scale.multiplyScalar(1.2);
+        if (myMesh.current.scale.x >= 1) {
+          setGrowObject(false);
+        }
+      }
     });
 
     return (
-      <mesh ref={myMesh}>
+      <mesh
+        ref={myMesh}
+        onClick={() => {
+          setShrinkObject(true);
+        }}
+      >
         <boxGeometry />
         <meshStandardMaterial />
       </mesh>
@@ -18,7 +38,7 @@ export default function ThreeRenders() {
   }
 
   return (
-    <div className="canvasContainer">
+    <div className="container">
       <Canvas>
         <directionalLight position={[0, 0, 5]} />
         <Box />
