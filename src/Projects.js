98414eb638cc87e7563811a1ca2910ec04838d0a
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2022-09-15 01:47:21 -0400

    fixed radom gap betwen projec title and projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,17 +1,24 @@
 import "./Project.css";
 import visualg from "./Project Pictures/visualg.png";
+import squadify from "./Project Pictures/squadify.png";
 export default function Projects() {
   return (
     <div>
       <div className="project">
         <h1 className="projectTitle">Projects</h1>
       </div>
-      <div>
+      <div className="tempProj">
         <Project
           name="Visu-Alg"
           image={visualg}
           desc="An interactive website for visualizing graph traversal algorithms built with React."
           link="www.visu-alg.com"
+        />
+        <Project
+          name="Squadify"
+          image={squadify}
+          desc="A Web App built to help users compare their spotify stats with their friends. Built with React, Flask, and Firebase."
+          link="https://github.com/ArsalaanAli/squadify"
         />
       </div>
     </div>
