fc42e85483afeffe579938ad45bc569051034ee2
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2022-09-15 22:07:13 -0400

    1.0

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,6 +4,10 @@ export default function AboutMe() {
     <div className="amContainer">
       <div>
         <h1 className="amTitle">About Me</h1>
+        <div className="amText">
+          Hi, my name is Arsalaan and I still have to finish this part of my
+          website haha...
+        </div>
       </div>
     </div>
   );
