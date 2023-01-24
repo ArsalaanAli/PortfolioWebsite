f75b3e2487dd784bbf3b8277f13d71075241ff5d
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2023-01-23 21:45:06 -0500

    Added more experiences to experience section

--- src/Experience.js
+++ src/Experience.js
@@ -42,7 +42,7 @@ const experiences = [
     date: "Nov. 2020 - May. 2021",
     achievements: [
       "Taught Programming fundamentals in Python to a live class",
-      " Created weekly lessons about Data structures and Algorithms",
+      "Created weekly lessons about Data structures and Algorithms",
       "Led a team of 4 instructors to plan an exciting and educational classroom experience",
     ],
   },
@@ -51,9 +51,37 @@ const experiences = [
     title: "Python Workshop Host",
     date: "2020",
     achievements: [
-      "Taught Programming fundamentals in Python to a live class",
-      " Created weekly lessons about Data structures and Algorithms",
-      "Led a team of 4 instructors to plan an exciting and educational classroom experience",
+      "Taught a workshop about the fundamentals of Python to a live class at a hackathon",
+      "Worked with coordinators to execute and live stream the workshop",
+    ],
+  },
+  {
+    name: "VMSS Debate",
+    title: "Software Developer",
+    date: "Feb. 2020 - Sep. 2021",
+    achievements: [
+      "Used Google Cloud Platform, Flask, and Python to implement a Python backend and create a digital database",
+      "Used Python to build a matchmaking system and user interface to facilitate club activities",
+      "Established a 100% automated system for record keeping and managing the club's data",
+    ],
+  },
+  {
+    name: "University of Windsor Programming Competition",
+    title: "Competitive Programmer",
+    date: "2019",
+    achievements: [
+      "Won 1st place out of 30+ teams",
+      "Used knowledge of data structures and algorithms to code solutions to complex problems in Python and C++",
+    ],
+  },
+  {
+    name: "ECOO Programming Competition",
+    title: "Competitive Programmer",
+    date: "2019",
+    achievements: [
+      "Won 2nd place",
+      "Used knowledge of data structures and algorithms to code solutions to complex problems in Python and C++",
+      "Qualified to Regional Competition",
     ],
   },
 ];
